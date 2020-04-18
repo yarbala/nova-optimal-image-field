05ee0b8652cf98032fbb8ee2e9b08c39581e4e7a
Ярослав <yarbala@gmail.com>
2020-04-18 20:44:38 +0200

    Remove not necessary files. Write simple working version.

--- resources/js/field.js
+++
@@ -1,5 +0,0 @@
-Nova.booting((Vue, router, store) => {
-  Vue.component('index-optimal-image', require('./components/IndexField'))
-  Vue.component('detail-optimal-image', require('./components/DetailField'))
-  Vue.component('form-optimal-image', require('./components/FormField'))
-})
--- src/OptimalImage.php
+++ src/OptimalImage.php
@@ -2,14 +2,86 @@
 
 namespace Yarbala\OptimalImage;
 
-use Laravel\Nova\Fields\Field;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Laravel\Nova\Fields\Image;
+use Spatie\ImageOptimizer\OptimizerChain as ImageOptimizer;
+use Storage;
 
-class OptimalImage extends Field
+/**
+ * Данный класс унаследован от станадартного класса Image. Его предназаначение, это переопределить процесс записи
+ * изображения таким образом, чтобы обработать изображение после его загрузки.
+ *
+ * Class OptimalImage
+ * @package Yarbala\OptimalImage
+ */
+class OptimalImage extends Image
 {
     /**
-     * The field's component.
+     * Функция переопределяет базовую из родительского классса для Image 'Fields/File.php'. Код взят из базовой функции
+     * и ы него добавлен вызов optimizeImage после загрузки.
      *
-     * @var string
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $requestAttribute
+     * @return string
      */
-    public $component = 'optimal-image';
+    protected function storeFile($request, $requestAttribute)
+    {
+        if (! $this->storeAsCallback) {
+            $fileName = $request->file($requestAttribute)->store($this->getStorageDir(), $this->getStorageDisk());
+            if ($fileName) {
+                try {
+                    $this->optimizeImage($fileName);
+                } catch (FileNotFoundException $e) {
+                }
+            }
+
+            return $fileName;
+        }
+
+        $fileName = $request->file($requestAttribute)->storeAs(
+            $this->getStorageDir(), call_user_func($this->storeAsCallback, $request), $this->getStorageDisk()
+        );
+
+        if ($fileName) {
+            try {
+                $this->optimizeImage($fileName);
+            } catch (FileNotFoundException $e) {
+                //
+            }
+        }
+
+        return $fileName;
+    }
+
+    /**
+     * Функция производящая оптимизацию изображение
+     *
+     * @param $fileName
+     * @throws FileNotFoundException
+     */
+    protected function optimizeImage($fileName)
+    {
+        $needsUploadBack = false;
+        $localDisk = 'local';
+        $disk = $this->getStorageDisk();
+
+        /*  Так как базовый компонент работает с классом Storage, а Spatie\ImageOptimizer работает с локальными файлами
+            мы выгрузим файл в локальное хранилище в случае надобности
+        */
+        if ($disk === $localDisk || Storage::disk($localDisk)->exists($fileName)) {
+            $path = Storage::disk($localDisk)->path($fileName);
+        } else {
+            Storage::disk($localDisk)->put($fileName, Storage::disk($disk)->get($fileName));
+            $path = Storage::disk($localDisk)->path($fileName);
+            $needsUploadBack = true;
+        }
+
+        // Оптимизация изображения
+        app(ImageOptimizer::class)->optimize($path);
+
+        // Если мы вгружали файл, загрузим его обратно
+        if ($needsUploadBack) {
+            Storage::disk($disk)->put($fileName, Storage::disk($localDisk)->get($fileName));
+        }
+    }
 }
